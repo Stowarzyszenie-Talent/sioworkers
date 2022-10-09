8424b095bfc6e5b39de08dd49a6f244442c3745e
pixl.dt <olaf.targowski@otsrv.net>
2022-10-09 17:19:52 +0200

    Use newer sio2jail and fix its executor.

--- sio/workers/executors.py
+++ sio/workers/executors.py
@@ -599,96 +599,96 @@ class Sio2JailExecutor(SandboxExecutor):
     REAL_TIME_LIMIT_ADDEND = 1000  # (in ms)
 
     def __init__(self):
-        super(Sio2JailExecutor, self).__init__('sio2jail_exec-sandbox')
+        super(Sio2JailExecutor, self).__init__('sio2jail_exec-sandbox-1.4.2')
 
     def _execute(self, command, **kwargs):
         options = []
-        options += ['-b', os.path.join(self.rpath, 'boxes/minimal') + ':/:ro']
+        options += ["-b", os.path.join(self.rpath, "boxes/minimal") + ":/:ro"]
         options += [
-            '--memory-limit',
-            str(kwargs['mem_limit'] or self.DEFAULT_MEMORY_LIMIT) + 'K',
+            "--memory-limit",
+            str(kwargs["mem_limit"] or self.DEFAULT_MEMORY_LIMIT) + "K",
         ]
         options += [
-            '--instruction-count-limit',
+            "--instruction-count-limit",
             str(
-                (kwargs['time_limit'] or self.DEFAULT_TIME_LIMIT)
+                (kwargs["time_limit"] or self.DEFAULT_TIME_LIMIT)
                 * self.INSTRUCTIONS_PER_VIRTUAL_SECOND
-                / 1000
+                // 1000
             ),
         ]
         options += [
-            '--rtimelimit',
+            "--rtimelimit",
             str(
-                (kwargs['time_limit'] or self.DEFAULT_TIME_LIMIT)
+                (kwargs["time_limit"] or self.DEFAULT_TIME_LIMIT)
                 * self.REAL_TIME_LIMIT_MULTIPLIER
                 + self.REAL_TIME_LIMIT_ADDEND
             )
-            + 'ms',
+            + "ms",
         ]
         options += [
-            '--output-limit',
-            str(kwargs['output_limit'] or self.DEFAULT_OUTPUT_LIMIT) + 'K',
+            "--output-limit",
+            str(kwargs["output_limit"] or self.DEFAULT_OUTPUT_LIMIT) + "K",
         ]
-        command = [os.path.join(self.rpath, 'sio2jail')] + options + ['--'] + command
+        command = [os.path.join(self.rpath, "sio2jail")] + options + ["--"] + command
 
         renv = {}
         try:
             result_file = tempfile.NamedTemporaryFile(dir=tempcwd())
-            kwargs['ignore_errors'] = True
+            kwargs["ignore_errors"] = True
             renv = execute_command(
-                command + [noquote('2>'), result_file.name], **kwargs
+                command + [noquote("2>"), result_file.name], **kwargs
             )
 
-            if renv['return_code'] != 0:
+            if renv["return_code"] != 0:
                 raise ExecError(
-                    'Sio2Jail returned code %s, stderr: %s'
-                    % (renv['return_code'], result_file.read(10240))
+                    "Sio2Jail returned code %s, stderr: %s"
+                    % (renv["return_code"], result_file.read(10240))
                 )
 
             result_file.seek(0)
             status_line = result_file.readline().strip().split()[1:]
-            renv['result_string'] = result_file.readline().strip()
+            renv["result_string"] = result_file.readline().strip()
             result_file.close()
             for num, key in enumerate(
-                ('result_code', 'time_used', None, 'mem_used', None)
+                ("result_code", "time_used", None, "mem_used", None)
             ):
                 if key:
                     renv[key] = int(status_line[num])
 
-            if renv['result_string'] == 'ok':
-                renv['result_code'] = 'OK'
-            elif renv['result_string'] == 'time limit exceeded':
-                renv['result_code'] = 'TLE'
-            elif renv['result_string'] == 'real time limit exceeded':
-                renv['result_code'] = 'TLE'
-            elif renv['result_string'] == 'memory limit exceeded':
-                renv['result_code'] = 'MLE'
-            elif renv['result_string'].startswith('intercepted forbidden syscall'):
-                renv['result_code'] = 'RV'
-            elif renv['result_string'].startswith('process exited due to signal'):
-                renv['result_code'] = 'RE'
+            if renv["result_string"] == b'ok':
+                renv["result_code"] = b'OK'
+            elif renv["result_string"] == b'time limit exceeded':
+                renv["result_code"] = b'TLE'
+            elif renv["result_string"] == b'real time limit exceeded':
+                renv["result_code"] = b'TLE'
+            elif renv["result_string"] == b'memory limit exceeded':
+                renv["result_code"] = b'MLE'
+            elif renv["result_string"].startswith(b'intercepted forbidden syscall'):
+                renv["result_code"] = b'RV'
+            elif renv["result_string"].startswith(b'process exited due to signal'):
+                renv["result_code"] = b'RE'
             else:
                 raise ExecError(
-                    'Unrecognized Sio2Jail result string: %s' % renv['result_string']
+                    "Unrecognized Sio2Jail result string: %s" % renv["result_string"]
                 )
 
         except (EnvironmentError, EOFError, RuntimeError) as e:
-            logger.error('Sio2JailExecutor error: %s', traceback.format_exc())
+            logger.error("Sio2JailExecutor error: %s", traceback.format_exc())
             logger.error(
-                'Sio2JailExecutor error dirlist: %s: %s',
+                "Sio2JailExecutor error dirlist: %s: %s",
                 tempcwd(),
                 str(os.listdir(tempcwd())),
             )
 
-            renv['result_code'] = 'SE'
-            for i in ('time_used', 'mem_used'):
+            renv["result_code"] = b'SE'
+            for i in ("time_used", "mem_used"):
                 renv.setdefault(i, 0)
-            renv['result_string'] = str(e)
+            renv["result_string"] = str(e)
 
-            if not kwargs.get('ignore_errors', False):
+            if not kwargs.get("ignore_errors", False):
                 raise ExecError(
-                    'Failed to execute command: %s. Reason: %s'
-                    % (command, renv['result_string'])
+                    "Failed to execute command: %s. Reason: %s"
+                    % (command, renv["result_string"])
                 )
 
         return renv
