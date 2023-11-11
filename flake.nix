2a9e14db8d95695de81ae5af2ab402c4315d7200
Olaf Targowski <me@otsrv.net>
2023-11-11 18:00:47 +0100

    [nix] Use the inputs' nixpkgs, not the system one
    This makes the nix package far more reproducible.

--- flake.nix
+++ flake.nix
@@ -1,45 +1,70 @@
 {
-  description = "Filetracker caching file storage";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+  description = "The task runner used by SIO2";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.filetracker.url = "github:Stowarzyszenie-Talent/filetracker";
+  inputs.filetracker = {
+    url = "github:Stowarzyszenie-Talent/filetracker";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, flake-utils, filetracker }: {
-    overlays.default = final: prev: {
-      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-        (python-final: python-prev: {
-          sioworkers = prev.callPackage ./nix/package.nix python-prev;
-        })
-      ];
-
-      sioworkers = with final.python310Packages; toPythonApplication sioworkers;
-    };
-
-    nixosModules.default = {
-      imports = [
-        filetracker.nixosModules.default
-        ./nix/module
-      ];
-    };
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-
-      modules = [
-        (_: {
-          nixpkgs.overlays = [
-            self.overlays.default
-            filetracker.overlays.default
-          ];
-        })
-        ./nix/container.nix
-      ];
-    };
-  } // (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+  outputs = { self, nixpkgs, flake-utils, filetracker }:
     let
-      pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default filetracker.overlays.default ]; };
+      importWithPin = file:
+        ({ pkgs, lib, config, ... }: import file {
+          # Use pinned nixpkgs from our input for epic reproducibility.
+          pkgs = import nixpkgs {
+            inherit (pkgs) system; overlays = [ self.overlays.default filetracker.overlays.default ];
+          };
+          inherit lib config;
+        });
+      selfImports = [
+        (importWithPin ./nix/module/worker.nix)
+        (importWithPin ./nix/module/workersd.nix)
+      ];
     in
     {
-      packages.default = pkgs.sioworkers;
-      devShell = pkgs.sioworkers;
-    }));
+      overlays.default = final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (python-final: python-prev: {
+            sioworkers = prev.callPackage ./nix/package.nix python-prev;
+          })
+        ];
+
+        sioworkers = with final.python310Packages; toPythonApplication sioworkers;
+      };
+
+      nixosModules.self = {
+        nixpkgs.overlays = [ self.overlays.default ];
+        imports = selfImports;
+      };
+
+      nixosModules.default = {
+        nixpkgs.overlays = [ self.overlays.default ];
+        imports = [ filetracker.nixosModules.default ] ++ selfImports;
+      };
+
+      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        nixosModules.self = {
+          nixpkgs.overlays = [ self.overlays.default ];
+          imports = selfImports;
+        };
+        modules = [
+          (_: {
+            nixpkgs.overlays = [
+              self.overlays.default
+              filetracker.overlays.default
+            ];
+          })
+          ./nix/container.nix
+        ];
+      };
+    } // (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default filetracker.overlays.default ]; };
+      in
+      {
+        packages.default = pkgs.sioworkers;
+        devShell = pkgs.sioworkers;
+      }));
 }
