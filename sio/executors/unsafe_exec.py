2504dbc1ec4c701cccbca7f0543bedcc35a307ee
Olaf Targowski <me@otsrv.net>
2025-06-17 21:40:02 +0200

    Fix s2j executor argument name usage

--- sio/executors/unsafe_exec.py
+++ sio/executors/unsafe_exec.py
@@ -5,7 +5,7 @@ from sio.workers.executors import Sio2JailExecutor
 
 def run(environ):
     # This is a bit hacky patch to make unsafe-exec safe
-    return common.run(environ, Sio2JailExecutor(use_perf=False))
+    return common.run(environ, Sio2JailExecutor(measure_real_time=True))
 
 def interactive_run(environ):
-    return interactive_common.run(environ, Sio2JailExecutor(use_perf=False))
+    return interactive_common.run(environ, Sio2JailExecutor(measure_real_time=True))
